70d63288e39ff2daa0e36a7f91917cbde7355220
Vlad Vovk <vlad.vovk@datarobot.com>
2022-12-12 20:43:42 +0200

    added eslint + prettier

--- packages/api/src/routes.js
+++ packages/api/src/routes.js
@@ -3,14 +3,19 @@ import { getUserIdByName } from './twitter_fetcher.js';
 export function initRoutes(fastify) {
   // Declare a route
   fastify.get('/test', function (request, reply) {
-    reply.send({ message: 'Server running and looks healthy'});
+    reply.send({ message: 'Server running and looks healthy' });
   });
 
-  fastify.get('/check', async function(request, reply) {
+  fastify.get('/check', async function (request, reply) {
     const userName = request.query?.user_name ?? null;
-    if(!userName) {
-      fastify.log.error('ERROR: userName has not b een provided to check route');
-      const error = new Error('Twitter username has not been provided or wasn\'t parsed correctly');
+
+    if (!userName) {
+      fastify.log.error(
+        'ERROR: userName has not b een provided to check route'
+      );
+      const error = new Error(
+        'Twitter username has not been provided or was not parsed correctly'
+      );
       reply.code(400).send(error);
     } else {
       try {
@@ -22,5 +27,5 @@ export function initRoutes(fastify) {
         reply.code(e.status).send(`${e?.error?.title}. ${e?.error?.detail}`);
       }
     }
-  })
+  });
 }
